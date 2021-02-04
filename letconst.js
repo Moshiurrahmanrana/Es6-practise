fbad675ef759d65fe4dae218e9b99b23c7581c14
moshiurrahmanrana <moshiur19121997@gmail.com>
2021-02-04 20:09:40 +0600

    letConst

---
+++ letconst.js
@@ -0,0 +1,11 @@
+// const habby = "elish kanchon";
+// habby = "omor sunny"
+// console.log(habby);
+// const numbers = [43,23];
+// numbers[0] = 33;
+// numbers.push(55);
+// numbers.pop(33);
+// console.log(numbers);
+let patiensName = "akmol";
+patiensName = "rohim"
+console.log(patiensName);
